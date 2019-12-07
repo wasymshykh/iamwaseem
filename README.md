46c4ba4fbdb3dc290807f8e0a97762c5332d2417
root <root@vps62858.hostsailor.com>
2019-12-08 00:02:30 +0100

    initial

---
+++ README.md
@@ -0,0 +1,1 @@
+# iamwaseem.com website
---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+</head>
+<body>
+    
+
+    <div class="container">
+        <div class="row">
+
+            <div class="col-md-12 p-4">
+                <h3>Events following</h3>
+            </div>
+
+            <div class="col-md-12">
+                <div class="row">
+                    <table class="col-md-12 col-lg-8 offset-lg-2 table table-striped">
+                        <thead class="thead-dark">
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Link</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>ACM League</td>
+                                <td>28 Dec 2019</td>
+                                <td><a href="https://www.facebook.com/events/1293975794097714/" class="btn btn-primary">FB</a></td>
+                            </tr>
+                            <tr>
+                                <td>Code.play</td>
+                                <td>10 Jan 2019</td>
+                                <td><a href="https://www.facebook.com/events/1038224436516970/" class="btn btn-primary">FB</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+
+</body>
+</html>
