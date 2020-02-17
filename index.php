c85dcb83092e995dea4a47de743c53ec6be31697
Muhammad Waseem <wasymshykh@gmail.com>
2020-02-18 02:16:03 +0500

    new simple

--- index.php
+++ index.php
@@ -4,47 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Muhammad Waseem</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,500i,700,900&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css">
+
+
+    <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
     
 
-    <div class="container">
-        <div class="row">
+    <section class="mw-main mw-flex mw-flex--jc mw-flex--ai">
+        <div class="mw-midbox mw-flex">
+            <div class="mw-left-pic">
+                <img src="images/me.jpg" alt="Muhammad Waseem">
 
-            <div class="col-md-12 p-4">
-                <h3>Events following</h3>
-            </div>
-
-            <div class="col-md-12">
-                <div class="row">
-                    <table class="col-md-12 col-lg-8 offset-lg-2 table table-striped">
-                        <thead class="thead-dark">
-                            <th>Name</th>
-                            <th>Date</th>
-                            <th>Link</th>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>ACM League</td>
-                                <td>28 Dec 2019</td>
-                                <td><a href="https://www.facebook.com/events/1293975794097714/" class="btn btn-primary">FB</a></td>
-                            </tr>
-                            <tr>
-                                <td>Code.play</td>
-                                <td>10 Jan 2019</td>
-                                <td><a href="https://www.facebook.com/events/1038224436516970/" class="btn btn-primary">FB</a></td>
-                            </tr>
-                        </tbody>
-                    </table>
+                <div class="mw-left-contact mw-flex">
+                    <span><i class="fab fa-whatsapp-square"></i><a href="https://wa.me/923022736286" target="_blank">+92-302-2736286</a></span>
+                    <span><a href="https://opticaldot.com/" target="_blank"><i class="fas fa-globe"></i> I own this</a></span>
+                    <span><a href="https://www.facebook.com/wasymshykh" target="_blank"><i class="fab fa-facebook-square"></i> rarely use fb</a></span>
+                    <span><a href="https://www.linkedin.com/in/wasym" target="_blank"><i class="fab fa-linkedin"></i> do people use?</a></span>
+                    <span><a href="https://github.com/wasymshykh" target="_blank"><i class="fab fa-github-square"></i> everyone's fav</a></span>
+                    <span><a href="mailto:wasymshykh@gmail.com" target="_top"><i class="fas fa-envelope-square"></i> mostly active on gmail</a></span>
                 </div>
-
             </div>
+            <div class="mw-right-text">
+                <h1>Muhammad Waseem</h1>
+                <h3>UI/UX, Web Developer, Software Engineer</h3>
 
+                <p><span>About me</span> I'm proud Iqraian, studying Software Engineering learning blah blah. I'm into web since 2013. My goal is to be a system architect, traveller and graduate.</p>
+
+                <p><span>Events Attended</span> <i></i> GDG Kolachi DevFest, IU ICEEST</p>
+
+                <p><span>Competetions Participated</span> <i>I love working under limited time constraint</i> FAST-NUCES Procom, DSU Spectrum, IU Ecompetencia, Aptech CodeFest, HU Codeplay, IBA Probattle, UBIT Proquest</p>
+
+                <p><span>Favorite Movies</span> <i>I don't like watching movies anymore but these are some of my favorite ones:</i> Into the Wild, A beautiful mind, The Dark Knight, The Theory of Everything, Good Will Hunting, The Pursuit of Happyness,  </p>
+
+                <p><span>Favorite TV Series</span> <i>I procastinate on tv series</i> Silicon Valley, Breaking Bad, Better call saul, Friends, The Office</p>
+            </div>
         </div>
-    </div>
+    </section>
 
 
 </body>
